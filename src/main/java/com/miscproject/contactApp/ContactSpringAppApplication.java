bc37597c907b3eee9af766cf929fa452ae6ae658
Anthony <anthonymiller313@hotmail.com>
2019-06-21 00:53:11 -0400

    First init for contact app. Started on domain classes.

---
+++ src/main/java/com/miscproject/contactApp/ContactSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.miscproject.contactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactSpringAppApplication.class, args);
+	}
+
+}
