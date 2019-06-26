d25c6ded394c172e7a27cf2d744bc96f96cc8a70
Anthony <anthonymiller313@hotmail.com>
2019-06-26 01:15:44 -0400

    added MVC controller classes and command classes for end user functions.

---
+++ src/main/java/com/miscproject/contactApp/Command/UserCommand.java
@@ -0,0 +1,17 @@
+package com.miscproject.contactApp.Command;
+
+import com.miscproject.contactApp.Domain.User;
+
+public class UserCommand {
+	User user;
+	
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
